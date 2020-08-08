8e61559588ccd7e5d72390018928eb5d96336240
AvantSekai <avantsekai@gmail.com>
2020-08-07 21:43:18 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts"></section>
+    <PostPreview
+      id="1"
+      thumbnail="https://code.visualstudio.com/assets/docs/editor/integrated-terminal/integrated-terminal.png"
+      title="Hello there!"
+      previewText="This is my first post!"
+    />>
+    <PostPreview
+      id="2"
+      thumbnail="https://code.visualstudio.com/assets/docs/editor/integrated-terminal/integrated-terminal.png"
+      title="Post 2!"
+      previewText="This is my first post!"
+    />> <PostPreview />>
+  </div>
+</template>
+
+<script>
+  import PostPreview from "@/components/Posts/PostPreview";
+
+  export default {
+    components: {
+      PostPreview: PostPreview,
+    },
+  };
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>>
